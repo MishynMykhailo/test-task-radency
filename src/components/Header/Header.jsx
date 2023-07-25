2f6309affc334f93e25779ab0ace831391fb85a6
MishynMykhailo <misham18092001@gmail.com>
2023-07-26 00:44:58 +0300

    first commit
    install react app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <ul>
+        <li>Name</li>
+        <li>Created</li>
+        <li>Category</li>
+        <li>Content</li>
+        <li>Date</li>
+        <li>Edit</li>
+        <li>Arvice</li>
+        <li>Delete</li>
+      </ul>
+    </>
+  );
+}
+
+export default Header;
